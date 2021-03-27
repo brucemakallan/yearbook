81497f6a0ba47c974cdab7738c2ca47cc913d334
brucemakallan <brucemakallan@gmail.com>
2021-03-27 17:51:00 +0300

    Add meta tags

--- components/CustomHead/index.js
+++ components/CustomHead/index.js
@@ -4,15 +4,22 @@ const WEBSITE_TITLE = "Yearbook | madeyear.com";
 
 const CustomHead = ({ title=WEBSITE_TITLE, page }) => {
   return (
-    <div>
-      <Head>
-        <title>
-          {page ? `Yearbook | ${page}` : title}
-        </title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
-  )
+    <Head>
+      <title>
+        {page ? `Yearbook | ${page}` : title}
+      </title>
+
+      <link rel="icon" href="/favicon.ico" />
+
+      <meta key="theme" name="theme-color" content="#000000" />
+      <meta key="short-desc" name="Yearbook" content="Online Yearbook generator for Schools and Universities" />
+      <meta key="name" name="application-name" content="Yearbook madeyear.com" />
+      <meta key="description" name="description" content="Online Yearbook generator for Schools and Universities" />
+      <meta key="keywords" name="keywords" content="Yearbook, Year, Book, Madeyear, Made, School, University, Universities, Student, madeyear.com" />
+      <meta key="author" name="author" content="madeyear.com" />
+      <meta key="viewport" name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+  );
 };
 
 export default CustomHead;
