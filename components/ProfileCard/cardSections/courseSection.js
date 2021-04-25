075611c4cb01649f1ac8dc46030937075366b251
brucemakallan <brucemakallan@gmail.com>
2021-04-25 19:48:21 +0300

    Show NA for department and course in non-universities

--- components/ProfileCard/cardSections/courseSection.js
+++ components/ProfileCard/cardSections/courseSection.js
@@ -5,28 +5,35 @@ import Typography from '@material-ui/core/Typography';
 import BusinessIcon from '@material-ui/icons/Business';
 import HomeWorkIcon from '@material-ui/icons/HomeWork';
 import SchoolIcon from '@material-ui/icons/School';
+import { institutionTypes } from '../../../helpers/enums';
 
-const CourseSection = ({ classes, singleProfile }) => (
-  <div className={classes.courseSection}>
-    <div className={classes.infoBox}>
-      <BusinessIcon color="primary" />
-      <Typography variant="body2" color="primary">
-        {get(singleProfile, 'course.department.university.name')}
-      </Typography>
+const CourseSection = ({ classes, singleProfile }) => {
+  const isUniversity = (
+    singleProfile?.course?.department?.university?.classification === institutionTypes[0].value
+  );
+
+  return (
+    <div className={classes.courseSection}>
+      <div className={classes.infoBox}>
+        <BusinessIcon color="primary" />
+        <Typography variant="body2" color="primary">
+          {get(singleProfile, 'course.department.university.name')}
+        </Typography>
+      </div>
+      <div className={classes.infoBox}>
+        <HomeWorkIcon color="primary" />
+        <Typography variant="body2" color="primary">
+          {isUniversity ? get(singleProfile, 'course.department.name') : 'N/A'}
+        </Typography>
+      </div>
+      <div className={classes.infoBox}>
+        <SchoolIcon color="primary" />
+        <Typography variant="body2" color="primary">
+          {isUniversity ? get(singleProfile, 'course.name') : 'N/A'}
+        </Typography>
+      </div>
     </div>
-    <div className={classes.infoBox}>
-      <HomeWorkIcon color="primary" />
-      <Typography variant="body2" color="primary">
-        {get(singleProfile, 'course.department.name')}
-      </Typography>
-    </div>
-    <div className={classes.infoBox}>
-      <SchoolIcon color="primary" />
-      <Typography variant="body2" color="primary">
-        {get(singleProfile, 'course.name')}
-      </Typography>
-    </div>
-  </div>
-);
+  );
+};
 
 export default CourseSection;
